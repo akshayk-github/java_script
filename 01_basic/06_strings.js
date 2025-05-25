8f1f96962aedd54e449c855c4e7fce554d314789
akshayk-github <104121315+akshayk-github@users.noreply.github.com>
2025-05-25 11:58:40 +0000

    string complete

--- 01_basic/06_strings.js
+++ 01_basic/06_strings.js
@@ -3,10 +3,28 @@ let number = 9558481639
 
 console.log(`My name is ${name} and my phone number is ${number}`);
 
-const gameName = new String('Akshay Kumbhani')
+const gameName = new String('Akshay Pravinbhai Kumbhani')
 
-console.log(gameName[5]);
-console.log(gameName.__proto__);
-console.log(gameName.length);
-console.log(gameName.toUpperCase());
-console.log(gameName.charAt(9));
+// console.log(gameName[5]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(9));
+// console.log(gameName.indexOf('a'));
+
+const newGame = gameName.substring(2, 9)
+// console.log(newGame);
+
+const sliceGame = gameName.slice(-10, 8) // most usable
+// console.log(sliceGame);
+
+const newString = "    Hello     Akshay         "
+// console.log(newString);
+// console.log(newString.trim());
+
+const url = "https://akshay.com/akshay%20kumbhani"  // usable
+console.log(url.replace('%20', '_'));
+console.log(url.includes('akshay'));
+console.log(url.includes('diku'));
+
+console.log(gameName.split(' '));
