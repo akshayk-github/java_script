f5fa551f28077d40986c8d460be8a27ac73b6443
akshayk-github <104121315+akshayk-github@users.noreply.github.com>
2025-05-25 07:25:23 +0000

    String Half knowledge

---
+++ 01_basic/06_strings.js
@@ -0,0 +1,12 @@
+let name = 'Akshay'
+let number = 9558481639
+
+console.log(`My name is ${name} and my phone number is ${number}`);
+
+const gameName = new String('Akshay Kumbhani')
+
+console.log(gameName[5]);
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(9));
