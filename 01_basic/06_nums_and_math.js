9e8f03973a34dbc0dbaf29521f90d8161240d71b
akshayk-github <104121315+akshayk-github@users.noreply.github.com>
2025-05-25 19:21:26 +0000

    Math and Num

---
+++ 01_basic/06_nums_and_math.js
@@ -0,0 +1,41 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toString().slice(1, 3));
+// console.log(balance.toFixed(3)); // most usable
+
+const otherNumber = 401.8845
+// console.log(otherNumber.toPrecision(4));
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 56415445425
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString('en-In'));
+
+
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++ mathe ++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+// console.log(Math);
+// console.log(Math.abs(-6));
+// console.log(Math.round(-64254.78));
+// console.log(Math.ceil(1.1));
+// console.log(Math.floor(7.85));
+// console.log(Math.min(4,8,74,5,74,25,78,6,88,4,14,4,5,5,1,2));
+// console.log(Math.max(4,8,74,5,74,25,78,6,88,4,14,4,5,5,1,2));
+
+// console.log(Math.random());
+// console.log((Math.random()*10) + 1);
+// console.log(((Math.random()*10) + 1).toPrecision(2));
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
