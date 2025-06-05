4493ff6fe58c4ca6ac9eab551886fc0e2c264a78
akshayk-github <104121315+akshayk-github@users.noreply.github.com>
2025-06-05 18:58:47 +0000

    half function

---
+++ 03_basic/01_function.js
@@ -0,0 +1,27 @@
+function sayMyName() {
+    console.log("A");
+    console.log("k");
+    console.log("s");
+    console.log("h");
+    console.log("a");
+    console.log("y");
+}
+
+sayMyName()
+
+// function addTwoNumber(num1, num2) {
+//     // console.log(num1+num2);
+// }
+
+// addTwoNumber(3, "7")
+// addTwoNumber(3, 7)
+
+
+
+function addTwoNumber(num1, num2) {
+    let result = num1 + num2
+    return result
+}
+
+let result = addTwoNumber(3, 7)
+console.log("Result : ", result);
